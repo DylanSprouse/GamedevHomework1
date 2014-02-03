b459c7685b45a01cd96d98e4a383149100535dd1
Dylan Sprouse <dts264@nyu.edu>
2014-02-03 14:02:55 -0500

    Created this to aid in progression of my life which I don't have

---
+++ Assets/scripts/HelloWorld.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloWorld : MonoBehaviour {
+
+	// public makes a variable global, otherwise it defaults to private
+	public string message = "Hallo mein Freunde";
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ( "Hello World!" );
+
+		//Fur der Vatterlandeschaft (displays "hello world" in german in our 3d text
+		GetComponent<TextMesh>().text = message;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
